7e087c4e349f9d9399a5edc03e7d7440cf54b1af
Patrick <patrick.kuen11@gmail.com>
2018-05-22 10:35:37 +0200

    It is now also possible to import the component with Vue.use

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import VueFlowy from './VueFlowy.vue'
 import FlowChart from './FlowChart.js'
 
+const plugin = {
+  install: Vue => {
+    Vue.component(VueFlowy.name, VueFlowy)
+  }
+}
+
+VueFlowy.install = plugin.install
+
 export default {VueFlowy, FlowChart}
